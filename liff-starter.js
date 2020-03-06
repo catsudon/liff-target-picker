bc30b6b68f70899082a00dda73c84b2da7b86d80
Mark Sat <sathanalao@hotmail.com>
2020-03-07 00:43:11 +0700

    d

--- liff-starter.js
+++ liff-starter.js
@@ -26,14 +26,12 @@ function initializeApp(data) {
 
   // sendMessages call
   document.getElementById('sendmessagebutton').addEventListener('click', function () {
-    liff.shareTargetPicker([{
-          type: 'text',
-          text: "You've successfully sent a message! Hooray!"
-      }, {
-          type: 'sticker',
-          packageId: '2',
-          stickerId: '144'
-      }]).then(function () {
+    liff.shareTargetPicker([
+      {
+        type: "text",
+        text: "Hello, World!"
+      }
+    ]).then(function () {
           window.alert("Message sent");
       }).catch(function (error) {
           window.alert("Error sending message: " + error);
