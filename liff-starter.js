775a2463051e577e3d0d594c273509a2fa566993
Mark Sat <sathanalao@hotmail.com>
2020-03-07 00:07:08 +0700

    d

--- liff-starter.js
+++ liff-starter.js
@@ -1,6 +1,12 @@
-window.onload = function (){
-  liff.sendMessages([{
-    type: 'text',
-    text: "im gay"
-}])
-}
+liff.sendMessages([{
+  type: 'text',
+  text: "You've successfully sent a message! Hooray!"
+}, {
+  type: 'sticker',
+  packageId: '2',
+  stickerId: '144'
+}]).then(function () {
+  window.alert("Message sent");
+}).catch(function (error) {
+  window.alert("Error sending message: " + error);
+});
