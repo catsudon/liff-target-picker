48246b602d3d4e60b761051e367c759e26e87198
Mark Sat <sathanalao@hotmail.com>
2020-03-07 00:55:42 +0700

    d

--- liff-starter.js
+++ liff-starter.js
@@ -47,42 +47,42 @@ function initializeApp(data) {
       toggleAccessToken();
   });
 
-//   // get profile call
-//   document.getElementById('getprofilebutton').addEventListener('click', function () {
-//       liff.getProfile().then(function (profile) {
-//           document.getElementById('useridprofilefield').textContent = profile.userId;
-//           document.getElementById('displaynamefield').textContent = profile.displayName;
+  // get profile call
+  document.getElementById('getprofilebutton').addEventListener('click', function () {
+      liff.getProfile().then(function (profile) {
+          document.getElementById('useridprofilefield').textContent = profile.userId;
+          document.getElementById('displaynamefield').textContent = profile.displayName;
 
-//           const profilePictureDiv = document.getElementById('profilepicturediv');
-//           if (profilePictureDiv.firstElementChild) {
-//               profilePictureDiv.removeChild(profilePictureDiv.firstElementChild);
-//           }
-//           const img = document.createElement('img');
-//           img.src = profile.pictureUrl;
-//           img.alt = "Profile Picture";
-//           profilePictureDiv.appendChild(img);
+          const profilePictureDiv = document.getElementById('profilepicturediv');
+          if (profilePictureDiv.firstElementChild) {
+              profilePictureDiv.removeChild(profilePictureDiv.firstElementChild);
+          }
+          const img = document.createElement('img');
+          img.src = profile.pictureUrl;
+          img.alt = "Profile Picture";
+          profilePictureDiv.appendChild(img);
 
-//           document.getElementById('statusmessagefield').textContent = profile.statusMessage;
-//           toggleProfileData();
-//       }).catch(function (error) {
-//           window.alert("Error getting profile: " + error);
-//       });
-//   });
-// }
+          document.getElementById('statusmessagefield').textContent = profile.statusMessage;
+          toggleProfileData();
+      }).catch(function (error) {
+          window.alert("Error getting profile: " + error);
+      });
+  });
+}
 
-// function toggleAccessToken() {
-//   toggleElement('accesstokendata');
-// }
+function toggleAccessToken() {
+  toggleElement('accesstokendata');
+}
 
-// function toggleProfileData() {
-//   toggleElement('profileinfo');
-// }
+function toggleProfileData() {
+  toggleElement('profileinfo');
+}
 
-// function toggleElement(elementId) {
-//   const elem = document.getElementById(elementId);
-//   if (elem.offsetWidth > 0 && elem.offsetHeight > 0) {
-//       elem.style.display = "none";
-//   } else {
-//       elem.style.display = "block";
-//   }
-// }
+function toggleElement(elementId) {
+  const elem = document.getElementById(elementId);
+  if (elem.offsetWidth > 0 && elem.offsetHeight > 0) {
+      elem.style.display = "none";
+  } else {
+      elem.style.display = "block";
+  }
+}
