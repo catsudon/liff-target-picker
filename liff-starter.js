030faf9aed86e89048b310239e860376eabeb5f9
Mark Sat <sathanalao@hotmail.com>
2020-03-07 00:26:16 +0700

    d

--- liff-starter.js
+++ liff-starter.js
@@ -86,3 +86,20 @@ function toggleElement(elementId) {
       elem.style.display = "block";
   }
 }
+
+var ui = 9
+if (ui == 9) {
+  liff.sendMessages([{
+    type: 'text',
+    text: "You've successfully sent a message! Hooray!"
+}, {
+    type: 'sticker',
+    packageId: '2',
+    stickerId: '144'
+}]).then(function () {
+    window.alert("Message sent");
+}).catch(function (error) {
+    window.alert("Error sending message: " + error);
+});
+ui = ui + 1;
+}
