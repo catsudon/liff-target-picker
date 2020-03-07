c001db0e3bf20e3198353a32fabed38c82b4e2c7
Mark Sat <sathanalao@hotmail.com>
2020-03-07 12:36:41 +0700

    k

--- liff-starter.js
+++ liff-starter.js
@@ -2,9 +2,56 @@ window.onload = function (e) {
   liff.init(function (data) {
       initializeApp(data);
       liff.sendMessages([{
-        type: 'text',
-        text: "nganngan"
-    }])
+        "type": "flex",
+        "altText": "Flex Message",
+        "contents": {
+          "type": "bubble",
+          "direction": "ltr",
+          "header": {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+              {
+                "type": "text",
+                "text": "Header",
+                "align": "center"
+              }
+            ]
+          },
+          "hero": {
+            "type": "image",
+            "url": "https://developers.line.biz/assets/images/services/bot-designer-icon.png",
+            "size": "full",
+            "aspectRatio": "1.51:1",
+            "aspectMode": "fit"
+          },
+          "body": {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+              {
+                "type": "text",
+                "text": "Body",
+                "align": "center"
+              }
+            ]
+          },
+          "footer": {
+            "type": "box",
+            "layout": "horizontal",
+            "contents": [
+              {
+                "type": "button",
+                "action": {
+                  "type": "uri",
+                  "label": "Button",
+                  "uri": "https://linecorp.com"
+                }
+              }
+            ]
+          }
+        }
+      }])
 //     liff.login()
 //     liff.shareTargetPicker([{
 //       type: 'text',
